7dd57bfdc2865b92241f9a7b175831b390214988
erz <erz05er@gmail.com>
2013-10-25 23:28:15 -0800

    clients

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,3 +1,44 @@
 class ClientsController < ApplicationController
 	before_filter :authorize_access
+
+	def index
+		@clients = Client.all
+    end
+
+	def new
+		@client = Client.new	
+	end
+
+	def create
+		@client = Client.new(params[:client].permit(:name, :company, :email, :phone))
+		if @client.save
+			redirect_to @client
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		@client = Client.find(params[:id])
+	end
+
+	def update
+		@client = Client.find(params[:id])
+		if(@client.update(params[:client].permit(:name, :company, :email, :phone)))
+			redirect_to @client
+		else
+			render 'edit'
+		end
+	end
+
+	def show
+  		@client = Client.find(params[:id])
+	end
+
+	def destroy
+		@client = Client.find(params[:id])
+		@client.destroy
+
+		redirect_to clients_path
+	end
 end
